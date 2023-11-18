6bc1f98675f54da66874ec480a21fbe170f0395b
liayk0323 <wuhenry5@gmail.com>
2023-11-19 01:17:17 +0800

    new

--- exp1.py
+++ exp1.py
@@ -22,14 +22,12 @@ df_Y =df_Y.drop(df_Y[df_Y['country']==" ?"].index)
 df_Y =df_Y.drop(df_Y[df_Y['occupation']==" ?"].index)
 df_Y =df_Y.drop(df_Y[df_Y['marital']==" ?"].index)
 
-
-
 from sklearn.preprocessing import OneHotEncoder
 
 labelencoder = LabelEncoder()
 
 
-df_X["workclass"] = labelencoder.fit_transform(df_X["workclass"].astype('string')) # 將"gender"裡的字串自動轉換成數值
+df_X["workclass"] = labelencoder.fit_transform(df_X["workclass"].astype('string')) 
 df_Y["workclass"] = labelencoder.fit_transform(df_Y["workclass"].astype('string'))
 
 
@@ -69,13 +67,11 @@ y_test = df_Y['hr_per_week']
 X_test = df_Y.drop(columns=['hr_per_week'])
 
 
-
 from sklearn.preprocessing import StandardScaler
 #標準化
 ss = StandardScaler()
-ss.fit(X_train)
-X_train_std = ss.transform(X_train)
-X_test_std = ss.transform(X_test)
+X_train_std = ss.fit_transform(X_train)
+X_test_std = ss.fit_transform(X_test)
 
 #---------------------隨機森林-----------------------------------#
 from sklearn.ensemble import RandomForestRegressor
@@ -89,23 +85,19 @@ forest = RandomForestRegressor(n_estimators=25,
                                 min_samples_leaf=2)
 forest.fit(X_train, y_train)
 ff = forest.fit(X_train_std, y_train)
-s = sorted(zip(map(lambda x: round(x,4), ff.feature_importances_), names),reverse=True)
 f = forest.predict(X_test_std)
-#print('score: '+str(forest.score(X_test_std, y_test)))
 
-# X_combined = np.vstack((X_train, X_test))
-# y_combined = np.hstack((y_train, y_test))
 
 RMSE = metrics.mean_squared_error(f, y_test)**0.5
 MAE = metrics.mean_absolute_error(f, y_test)
 MAPE = metrics.mean_absolute_percentage_error(f, y_test)
-# MAPE = np.mean(np.abs((f-y_test)/f)) * 100
+
 print('隨機森林: ')
 print('RMSE: '+str(RMSE))
 print('MAE: '+str(MAE))
 print('MAPE: '+str(MAPE))
 
-#print(s)
+
 #---------------------XGBoost-------------------------------#
 
 from xgboost import XGBRegressor
@@ -121,12 +113,10 @@ xgb = XGBRegressor(learning_rate=0.1,
                     max_depth=5,
                     min_child_weight=1)
 
-
 xx = xgb.fit(X_train, y_train)
-s = sorted(zip(map(lambda x: round(x,4), xx.feature_importances_), names),reverse=True)
+
 x = xgb.predict(X_test)
 print('score: '+str(xgb.score(X_test, y_test)))
-
 
 RMSE = metrics.mean_squared_error(x, y_test)**0.5
 MAE = metrics.mean_absolute_error(x, y_test)
@@ -136,8 +126,6 @@ print('RMSE: '+str(RMSE))
 print('MAE: '+str(MAE))
 print('MAPE: '+str(MAPE))
 
-
-
 #---------------------SVR-----------------------------------#
 
 from sklearn.svm import SVR
@@ -146,15 +134,12 @@ from sklearn.inspection import permutation_importance
 from matplotlib import *
 import matplotlib.pyplot as pyplot
 
-svr = SVR(epsilon=0.2, C=1.0)
-#因為反覆计算都計算不出最優超平面，所以將模型中的參數max_iter設置一下，默认为-1，也就是計算出直到收斂的平面時，改成較小的次數就可以解決。
-svr = svm.SVR(epsilon=0.2, C=1.0, max_iter=1000)
+svr = svm.SVR(epsilon=0.4, C=1.0, max_iter=500)
 
 svr.fit(X_train_std, y_train)
 s = svr.predict(X_test_std)
 print('SVR score:')
 print(svr.score(X_test_std, y_test))
-
 
 RMSE = metrics.mean_squared_error(s, y_test)**0.5
 MAE = metrics.mean_absolute_error(s, y_test)
@@ -164,34 +149,10 @@ print('RMSE: '+str(RMSE))
 print('MAE: '+str(MAE))
 print('MAPE: '+str(MAPE))
 
-model = SVR(max_iter=100)
-# fit the model
-model.fit(X_train_std, y_train)
-# perform permutation importance
-results = permutation_importance(model, X_train_std, y_train, scoring='neg_mean_squared_error')
-# get importance
-importance = results.importances_mean
-# summarize feature importance
-##for i,v in enumerate(importance):
-##    print('Feature: %0d, Score: %.5f' % (i,v))
-# plot feature importance
-pyplot.bar([x for x in range(len(importance))], importance)
-pyplot.show()
-
 #---------------------KNN-----------------------------------#
-
-from sklearn.preprocessing import StandardScaler
-#標準化
-ss = StandardScaler()
-ss.fit(X_train)
-X_train_std = ss.transform(X_train)
-X_test_std = ss.transform(X_test)
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import metrics
-
-
-#p=2歐式距離
 
 knn = KNeighborsRegressor(n_neighbors=10,
                            p=2, 
